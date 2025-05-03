c78c3eef9107e1fbe42c89c4109c5407f5bc5852
Seray Ayakta <serayayakta@sabanciuniv.edu>
2025-05-03 19:08:21 +0300

    fix: temporary solution for hydration error on console

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
 import { ReactQueryClientProvider } from "@/providers/ReactQueryClientProvider";
 import { CartProvider } from "@/context/CartProvider";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Kesik Bıçakçılık",
@@ -22,9 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`bg-gray-50 text-gray-900`}>
         <ReactQueryClientProvider>
           <CartProvider>{children}</CartProvider>
         </ReactQueryClientProvider>
